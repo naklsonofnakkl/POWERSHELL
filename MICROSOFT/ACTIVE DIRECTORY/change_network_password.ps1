e955481bcf7fa78f5d5ec96c8beb1cd16dd7aa6a
Andrew Wilson <naklsonofnakkl@gmail.com>
2022-05-20 19:53:32 -0400

    Update change_network_password.ps1
    - Removed some of the "Human Element"
    - Fixed the broken Set-ADAccountPassword command
    - updated the 'fail-safe' to match 'Else' in event If command fails

--- MICROSOFT/ACTIVE DIRECTORY/change_network_password.ps1
+++ MICROSOFT/ACTIVE DIRECTORY/change_network_password.ps1
@@ -5,16 +5,16 @@
 #Necessary moduel to interact with credential manager
 #Setting PSGallery as trusted to install the module and back
 #to untrusted for security reasons
-Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted -ErrorAction -SilentlyContinue
-Install-Module -Name CredentialManager -Scope CurrentUser -ErrorAction -SilentlyContinue
-Set-PSRepository -Name 'PSGallery' -InstallationPolicy Untrusted -ErrorAction -SilentlyContinue
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+Install-Module -Name CredentialManager -Scope CurrentUser
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Untrusted
 
-#Prompt the user for username and new NewPassword
-$user = Read-Host "Please enter your domain username [ex. JohnDoe, JDoe] and press enter..."
-$newPass = Read-Host "Please enter your new Password [ex. 78!Flapy] and press enter..." -AsSecureString
+#Prompt the user for username and old password
+$user = $env:UserName
+$oldPass = Read-Host "Please enter your CURRENT Password and press enter..." -AsSecureString
 
-#Change users password
-if (Set-ADAccountPassword -Identity $user -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "$newPass" -Force) -ErrorAction SilentlyContinue)
+#Change local users password
+if (Set-ADAccountPassword -Identity $user -OldPassword $oldPass)
 {
 #inform user password change complete
 #change the global protect password stored in credential manager
@@ -22,30 +22,44 @@ if (Set-ADAccountPassword -Identity $user -Reset -NewPassword (ConvertTo-SecureS
 #Update Group Policy with new Password
 Write-Host "Password has been updated successfully!"
 sleep 1
+$NewPassword = (Read-Host -Prompt "Please enter your NEW Password and press enter..." -AsSecureString)
 Write-Host 'Updating Global Protect Credentials...'
-New-StoredCredential -Target gpcp/LatestCP -Username $user -Password $newPass
+New-StoredCredential -Target gpcp/LatestCP -Username $user -Password $NewPassword
 Write-Host 'Restarting Global Protect...'
 Remove-Variable user
-Remove-Variable newPass
+Remove-Variable oldPass
+Remove-Variable NewPassword
 Stop-Process -Name 'PanGPS' -ErrorAction SilentlyContinue
 Stop-Process -Name 'PanGPA' -ErrorAction SilentlyContinue
 start-process -FilePath "C:\Program Files\Palo Alto Networks\GlobalProtect\PanGPA.exe"
+sleep 10
 Read-Host -Prompt "Press enter to continue once Global Protect is Connected..."
 sleep 1
 Write-Host 'Updating Group Policy...'
 Invoke-GPUpdate -Force
+Write-Host "Password Change Process has been Completed Successfully! Exiting..."
+sleep 3
+exit
 }
 Else {
-#Password changed failed. User needs to reach out to IT Support.
+  #Password changed failed. User needs to reach out to IT Support.
+  Remove-Variable user
+  Remove-Variable oldPass
+  Remove-Variable NewPassword
+  Write-Host "Password has FAILED to update!"
+  sleep 1
+  Write-Host "Please reach out to IT Support to have the password changed manually..."
+  Write-Host "Closing..."
+  sleep 5
+  exit
+}
+#In the event of a permission error accessing Set-ADAccountPassword
 Remove-Variable user
-Remove-Variable newPass
+Remove-Variable oldPass
+Remove-Variable NewPassword
 Write-Host "Password has FAILED to update!"
 sleep 1
 Write-Host "Please reach out to IT Support to have the password changed manually..."
 Write-Host "Closing..."
 sleep 5
-exit
-}
-Write-Host "Password Change Process has been Completed Successfully! Exiting..."
-sleep 3
 exit
