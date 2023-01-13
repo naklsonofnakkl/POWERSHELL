69d1fada354edf6eedc8f01c3188630c37eb13a3
Andrew Wilson <naklsonofnakkl@gmail.com>
2023-01-13 18:54:03 -0500

    Create Outlook_Navigation_bar.ps1
    Needed to make this in a hurry. Will come back to clean it up later I promise!

---
+++ MICROSOFT/OFFICE/OUTLOOK/Outlook_Navigation_bar.ps1
@@ -0,0 +1,16 @@
+## This script will alter the registry in order to put back the OUTLOOK
+## Navigation bar to the bottom of the screen as it previously NewPassword
+
+$regPath = 'HKCU:\Software\Microsoft\Office\16.0\Common\ExperimentEcs\Overrides'
+$regProperty = 'Microsoft.Office.Outlook.Hub.HubBar'
+$property = (Get-ItemProperty -Path $regPath).Microsoft.Office.Outlook.Hub.HubBar
+
+Push-Location
+Set-Location $regPath
+Set-ItemProperty . Microsoft.Office.Outlook.Hub.HubBar "False"
+
+Write-Host "Currently the value is set to..."
+$null -ne $property
+Write-Host "Closing script..."
+Pop-Location
+Sleep 10
