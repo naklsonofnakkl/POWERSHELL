f342626176cf5a109935e9e11fb262f073f93a75
Andrew Wilson <naklsonofnakkl@gmail.com>
2023-03-17 22:56:26 -0700

    Created two new scripts v0.01

---
+++ MICROSOFT/OFFICE/EXCEL/BBE.psm1
@@ -0,0 +1,74 @@
+<#
+.NOTES
+    Author: Andrew Wilson
+    Version: 0.0.1
+    
+.LINK
+    https://github.com/naklsonofnakkl/POWERSHELL
+
+.SYNOPSIS
+    This is a huge list of Excel functions necessary for various script I use in my bag of tricks
+.DESCRIPTION
+    - it is a lot of functions
+    - Can't even justify it really, just need to call 
+    - a lot of damn functions
+#>
+
+function Format-SteamCsv {
+
+    #set the output paths
+    $csvFilePath = "$env:TEMP\SteamLib_temp\steam_library_stats.csv"
+    $outputFilePath = "$env:TEMP\SteamLib_temp\$yourName.csv"
+
+    # Define the $yourName varible via a text prompt
+    Add-Type -AssemblyName System.Windows.Forms
+
+    $Form = New-Object System.Windows.Forms.Form
+    $Form.Text = "Enter Your Username"
+    $Form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog
+    $Form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
+    $Form.AutoSize = $true
+    $Form.AutoSizeMode = "GrowAndShrink"
+
+    # Create the message label
+    $MessageLabel = New-Object System.Windows.Forms.Label
+    $MessageLabel.Location = New-Object System.Drawing.Point(10, 10)
+    $MessageLabel.Size = New-Object System.Drawing.Size(280, 20)
+    $MessageLabel.Text = "Please enter your Online Alias:"
+    $Form.Controls.Add($MessageLabel)
+
+    # Create the API Key textbox
+    $yourNameTextBox = New-Object System.Windows.Forms.TextBox
+    $yourNameTextBox.Location = New-Object System.Drawing.Point(10, 30)
+    $yourNameTextBox.Size = New-Object System.Drawing.Size(280, 20)
+    $Form.Controls.Add($yourNameTextBox)
+
+    # Create the "Submit" button
+    $SubmitButton = New-Object System.Windows.Forms.Button
+    $SubmitButton.Location = New-Object System.Drawing.Point(115, 70)
+    $SubmitButton.Size = New-Object System.Drawing.Size(75, 23)
+    $SubmitButton.Text = "Submit"
+    $SubmitButton.Enabled = $false
+    $SubmitButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $Form.AcceptButton = $NSubmitButton
+    $Form.Controls.Add($SubmitButton)
+
+    # Show the form and wait for a result
+    $Result = $Form.ShowDialog()
+
+    # If the "Next" button was clicked and the API Key is 32 characters long, set the $yourName variable
+    if ($Result -eq [System.Windows.Forms.DialogResult]::OK) {
+        $yourName = $yourNameTextBox.Text
+
+        # Import the CSV file
+        $data = Import-Csv $csvFilePath
+
+        #Filter the data to only include rows with "true" in the 5th column
+        $data = $data | Where-Object { $_.Column5 -eq "true" }
+
+        #Export the updated data to a new CSV file
+        $data | Export-Csv $outputFilePath -NoTypeInformation
+    } else { 
+        exit
+    }
+}
---
+++ MICROSOFT/WINDOWS/STEAM/Steam_Library.ps1
@@ -0,0 +1,164 @@
+<#
+.NOTES
+    Author: Andrew Wilson
+    Version: 0.0.1
+    
+.LINK
+    https://github.com/naklsonofnakkl/POWERSHELL
+
+.SYNOPSIS
+    Script to download entire Steam Library that are Multiplayer
+.DESCRIPTION
+    - Promps the user for their API key and SteamID64
+#>
+
+
+
+# VARIBALE PARADISE!
+# create a temp folder
+# THIS IS WHERE LOGS WILL ALSO BE LOCATED!
+# C:\Users\[USERNAME]\AppData\Local\Temp\SteamLib_temp
+$tempDir = New-Item -ItemType Directory -Path $env:TEMP -Name SteamLib_temp -Force
+$appDownloadPath = $tempDir
+$appDownload = $appDownloadPath
+$appLogs = "$appDownload\SteamLib.log"
+
+# LOGS!!
+$ErrorActionPreference = "Stop"
+Start-Transcript -Path $appLogs -Append
+
+# Modules to Import!
+Import-Module -Name https://
+
+
+
+
+
+
+
+Add-Type -AssemblyName System.Windows.Forms
+
+$Form = New-Object System.Windows.Forms.Form
+$Form.Text = "Enter API Key"
+$Form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog
+$Form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
+$Form.AutoSize = $true
+$Form.AutoSizeMode = "GrowAndShrink"
+
+# Create the message label
+$MessageLabel = New-Object System.Windows.Forms.Label
+$MessageLabel.Location = New-Object System.Drawing.Point(10, 10)
+$MessageLabel.Size = New-Object System.Drawing.Size(280, 20)
+$MessageLabel.Text = "Please enter your API Key (must be 32 characters):"
+$Form.Controls.Add($MessageLabel)
+
+# Create the API Key textbox
+$ApiKeyTextBox = New-Object System.Windows.Forms.TextBox
+$ApiKeyTextBox.Location = New-Object System.Drawing.Point(10, 30)
+$ApiKeyTextBox.Size = New-Object System.Drawing.Size(280, 20)
+$Form.Controls.Add($ApiKeyTextBox)
+
+# Create the "Next" button
+$NextButton = New-Object System.Windows.Forms.Button
+$NextButton.Location = New-Object System.Drawing.Point(115, 70)
+$NextButton.Size = New-Object System.Drawing.Size(75, 23)
+$NextButton.Text = "Next"
+$NextButton.Enabled = $false
+$NextButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$Form.AcceptButton = $NextButton
+$Form.Controls.Add($NextButton)
+
+# Create the event handler for the API Key textbox
+ $ApiKeyTextBox.add_TextChanged({
+        if ($ApiKeyTextBox.Text.Length -eq 32) {
+            $NextButton.Enabled = $true
+        }
+        else {
+            $NextButton.Enabled = $false
+        }
+    })
+
+# Show the form and wait for a result
+$Result = $Form.ShowDialog()
+
+# If the "Next" button was clicked and the API Key is 32 characters long, set the $ApiKey variable
+if ($Result -eq [System.Windows.Forms.DialogResult]::OK -and $ApiKeyTextBox.Text.Length -eq 32) {
+    $Apikey = $ApiKeyTextBox.Text
+    Write-Host "$Apikey"
+    Add-Type -AssemblyName System.Windows.Forms
+
+    $Form = New-Object System.Windows.Forms.Form
+    $Form.Text = "Enter SteamID64"
+    $Form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog
+    $Form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
+    $Form.AutoSize = $true
+    $Form.AutoSizeMode = "GrowAndShrink"
+
+    # Create the message label
+    $MessageLabel = New-Object System.Windows.Forms.Label
+    $MessageLabel.Location = New-Object System.Drawing.Point(10, 10)
+    $MessageLabel.Size = New-Object System.Drawing.Size(280, 20)
+    $MessageLabel.Text = "Please enter your SteamID64 (must be 17 characters):"
+    $Form.Controls.Add($MessageLabel)
+
+    # Create the API Key textbox
+    $SteamID64TextBox = New-Object System.Windows.Forms.TextBox
+    $SteamID64TextBox.Location = New-Object System.Drawing.Point(10, 30)
+    $SteamID64TextBox.Size = New-Object System.Drawing.Size(280, 20)
+    $Form.Controls.Add($SteamID64TextBox)
+
+    # Create the "Next" button
+    $NextButton = New-Object System.Windows.Forms.Button
+    $NextButton.Location = New-Object System.Drawing.Point(115, 70)
+    $NextButton.Size = New-Object System.Drawing.Size(75, 23)
+    $NextButton.Text = "Next"
+    $NextButton.Enabled = $false
+    $NextButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $Form.AcceptButton = $NextButton
+    $Form.Controls.Add($NextButton)
+
+    # Create the event handler for the API Key textbox
+    $SteamID64TextBox.add_TextChanged({
+            if ($SteamID64TextBox.Text.Length -eq 17) {
+                $NextButton.Enabled = $true
+            }
+            else {
+                $NextButton.Enabled = $false
+            }
+        })
+
+    # Show the form and wait for a result
+    $Result = $Form.ShowDialog()
+
+    # If the "Next" button was clicked and the API Key is 32 characters long, set the $ApiKey variable
+    if ($Result -eq [System.Windows.Forms.DialogResult]::OK -and $SteamID64TextBox.Text.Length -eq 17) {
+        $SteamID64 = $SteamID64TextBox.Text
+        Write-Host "$SteamID64"
+
+        # Fetch the list of owned games
+        $owned_games_url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=$Apikey&steamid=$SteamID64&include_appinfo=1&format=json"
+        $owned_games_response = Invoke-RestMethod -Uri $owned_games_url
+        $owned_games = $owned_games_response.response.games
+        Write-Host "I HAS THE GAMES!"
+
+        # Function to check if a game has community visible stats
+        function HasCommunityVisibleStats($app_id) {
+            $store_url = "https://store.steampowered.com/api/appdetails?appids=$app_id"
+            $store_response = Invoke-RestMethod -Uri $store_url
+            $details = $store_response.$app_id.data
+            if ($details.categories.description -contains "Stats") {
+                return $details.has_community_visible_stats
+            }
+            return $false
+        }
+
+        # Export the game data to a CSV file
+        $owned_games | Export-Csv -Path "$appDownloadPath\steam_library_stats.csv" -NoTypeInformation
+
+        Format-SteamCsv
+    }
+
+}
+
+
+
