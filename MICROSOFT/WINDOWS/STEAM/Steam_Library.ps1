6f62703bcb802ba0722dcfadd0162fed233b0457
Andrew Wilson <naklsonofnakkl@gmail.com>
2023-03-19 14:26:53 -0700

    made things more secure v0.1.1

--- MICROSOFT/WINDOWS/STEAM/Steam_Library.ps1
+++ MICROSOFT/WINDOWS/STEAM/Steam_Library.ps1
@@ -1,18 +1,21 @@
+#Requires -RunAsAdministrator
+
 <#
 .NOTES
     Author: Andrew Wilson
-    Version: 0.1.0
+    Version: 0.1.1
     
 .LINK
     https://github.com/naklsonofnakkl/POWERSHELL
 
 .SYNOPSIS
-    Script to download entire Steam Library that are Multiplayer
+    Script to download entire Steam Library
 .DESCRIPTION
     - Promps the user for their API key and SteamID64
 #>
 
 # VARIABLE PARADISE
+
 # create a temp folder & Start log file
 # THIS IS WHERE LOGS WILL ALSO BE LOCATED!
 # C:\Users\[USERNAME]\AppData\Local\Temp\SteamLib_temp
@@ -25,11 +28,36 @@ Start-Transcript -Path $appLogs -Append
 
 # Grab the user's name for json file nameing
 $username = Split-Path $env:USERPROFILE -Leaf
-Write-Host "The current user is: $username"
+Write-Log "The current user is: $username"
 
 
 
 # FUNCTION JUNCTION!!
+
+function Set-ExecutionPolicy {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$policy
+    )
+ 
+    # Get the current execution policy
+    $originalPolicy = Get-ExecutionPolicy
+ 
+    try {
+        # Set the execution policy to the specified value
+        Set-ExecutionPolicy -ExecutionPolicy $policy -Scope CurrentUser -Force
+ 
+        # Run the script block
+        & $scriptblock
+    }
+    finally {
+        # Set the execution policy back to the original value
+        Set-ExecutionPolicy -ExecutionPolicy $originalPolicy -Scope CurrentUser -Force
+    }
+}
+
+# Function that runs whenever script ends
 function Clear-Installation {
     # Dispose of any forms
     $form.Dispose()
@@ -48,45 +76,51 @@ function Clear-Installation {
     }
 }
 
+#Install NuGet if it isn't already installed
 function Get-NuGet {
     $packageName = "NuGet"
     $version = "2.8.5.208"
     if (Get-Package -Name $packageName -ErrorAction SilentlyContinue | Where-Object { $_.Version -eq $version }) {
-        Write-Host "NuGet is Installed!"
+        Write-Log "NuGet is Installed!"
     }
     else {
-        Write-Host "Installing NuGet!"
+        Write-Log "Installing NuGet!"
         Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
         Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
     }
 }
 
+# Redirect Write-Host to log file
+function Write-Log {
+    param([string]$message)
+
+    # Get the current date and time in a specific format
+    $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+    # Write the message to the log file with the current date and time
+    "$date - $message" | Out-File -FilePath $tempDir -Append
+}
+
+# install modules, import them and then export JSON file
 function Join-JsonTable {
-
-    
-    # Install the ImportExcel and PSWriteExcel modules if they're not already installed
-    Install-Module -Name ImportExcel, PSWriteExcel
-
-    # Load the modules into the current session
-    Import-Module -Name ImportExcel, PSWriteExcel
-
     # Import the JSON data
     $Json_Url = $apiUrl
     $owned_games_Json = "$tempDir\$username.json"
     Invoke-WebRequest -Uri $Json_Url -OutFile $owned_games_Json
-    Write-Host "EXPORTING THE GAMES!"
+    Write-Log "EXPORTING THE GAMES!"
 }
 
-#Use the Steam API to create a varible with all the users games listed
-function Get-SteamLibrary {
-    # Fetch the list of owned games
-    $owned_games_url = $apiUrl
-    $owned_games = $owned_games_response.response.games
-    Write-Host "I HAS THE GAMES!"
-    
+function Get-ModuleExcel {
+    # Install the ImportExcel and PSWriteExcel modules if they're not already installed
+    Write-Log "INSTALLING MODULES!"
+    Install-Module -Name ImportExcel, PSWriteExcel
+    # Load the modules into the current session
+    Write-Log "IMPORTING MODULES!"
+    Import-Module -Name ImportExcel, PSWriteExcel
 }
 
-function Add-ModuleExcel {
+# validate if ImportExcel is available, otherwise install Nuget
+function Find-ModuleExcel {
 
     $moduleName = "ImportExcel"
     
@@ -94,82 +128,27 @@ function Add-ModuleExcel {
     $installedModule = Get-Module -ListAvailable | Where-Object { $_.Name -eq $moduleName }
         
     if ($installedModule) {
-        Write-Host "The $moduleName module is installed."
-    
+        Write-Log "The $moduleName module is installed."
         # execute the rest of the script 
-        Join-JsonTable
-        Write-Host "EXPORTED THE GAMES!"
-        Invoke-Item -Path $tempDir
-        Clear-Installation
     }
     else {
-        Write-Host "The $moduleName module is not installed."
+        Write-Log "The $moduleName module is not installed."
         # Install the missing module and run the rest of the script
         Get-NuGet
-        Write-Host "INSTALLED NUGET!"
-        Join-JsonTable
-        Write-Host "EXPORTED THE GAMES!"
-        Invoke-Item -Path $tempDir
-        Clear-Installation
+        Write-Log "INSTALLED NUGET!"
+        Get-ModuleExcel
+        Write-Log "IMPORTED MODULES!"
     }
     
 } 
 
-
-#This is a form to ask for the users API and SteamID
-Add-Type -AssemblyName System.Windows.Forms
-
-$Form = New-Object System.Windows.Forms.Form
-$Form.Text = "Enter API Key"
-$Form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog
-$Form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
-$Form.AutoSize = $true
-$form.TopMost = $true
-$Form.AutoSizeMode = "GrowAndShrink"
-
-# Create the message label
-$MessageLabel = New-Object System.Windows.Forms.Label
-$MessageLabel.Location = New-Object System.Drawing.Point(10, 10)
-$MessageLabel.Size = New-Object System.Drawing.Size(280, 20)
-$MessageLabel.Text = "Please enter your API Key (must be 32 characters):"
-$Form.Controls.Add($MessageLabel)
-
-# Create the API Key textbox
-$ApiKeyTextBox = New-Object System.Windows.Forms.TextBox
-$ApiKeyTextBox.Location = New-Object System.Drawing.Point(10, 30)
-$ApiKeyTextBox.Size = New-Object System.Drawing.Size(280, 20)
-$Form.Controls.Add($ApiKeyTextBox)
-
-# Create the "Next" button
-$NextButton = New-Object System.Windows.Forms.Button
-$NextButton.Location = New-Object System.Drawing.Point(115, 70)
-$NextButton.Size = New-Object System.Drawing.Size(75, 23)
-$NextButton.Text = "Next"
-$NextButton.Enabled = $false
-$NextButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
-$Form.AcceptButton = $NextButton
-$Form.Controls.Add($NextButton)
-
-# Create the event handler for the API Key textbox
-$ApiKeyTextBox.add_TextChanged({
-        if ($ApiKeyTextBox.Text.Length -eq 32) {
-            $NextButton.Enabled = $true
-        }
-        else {
-            $NextButton.Enabled = $false
-        }
-    })
-
-# Show the form and wait for a result
-$Result = $Form.ShowDialog()
-
-# If the "Next" button was clicked and the API Key is 32 characters long, set the $ApiKey variable
-if ($Result -eq [System.Windows.Forms.DialogResult]::OK -and $ApiKeyTextBox.Text.Length -eq 32) {
-    $Apikey = $ApiKeyTextBox.Text
+# Prompt the user for the API and SteamID64 keys
+function Get-ApiAccess {
+    #This is a form to ask for the users API and SteamID
     Add-Type -AssemblyName System.Windows.Forms
 
     $Form = New-Object System.Windows.Forms.Form
-    $Form.Text = "Enter SteamID64"
+    $Form.Text = "Enter API Key"
     $Form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog
     $Form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
     $Form.AutoSize = $true
@@ -180,14 +159,14 @@ if ($Result -eq [System.Windows.Forms.DialogResult]::OK -and $ApiKeyTextBox.Text
     $MessageLabel = New-Object System.Windows.Forms.Label
     $MessageLabel.Location = New-Object System.Drawing.Point(10, 10)
     $MessageLabel.Size = New-Object System.Drawing.Size(280, 20)
-    $MessageLabel.Text = "Please enter your SteamID64 (must be 17 characters):"
+    $MessageLabel.Text = "Please enter your API Key (must be 32 characters):"
     $Form.Controls.Add($MessageLabel)
 
     # Create the API Key textbox
-    $SteamID64TextBox = New-Object System.Windows.Forms.TextBox
-    $SteamID64TextBox.Location = New-Object System.Drawing.Point(10, 30)
-    $SteamID64TextBox.Size = New-Object System.Drawing.Size(280, 20)
-    $Form.Controls.Add($SteamID64TextBox)
+    $ApiKeyTextBox = New-Object System.Windows.Forms.TextBox
+    $ApiKeyTextBox.Location = New-Object System.Drawing.Point(10, 30)
+    $ApiKeyTextBox.Size = New-Object System.Drawing.Size(280, 20)
+    $Form.Controls.Add($ApiKeyTextBox)
 
     # Create the "Next" button
     $NextButton = New-Object System.Windows.Forms.Button
@@ -200,8 +179,8 @@ if ($Result -eq [System.Windows.Forms.DialogResult]::OK -and $ApiKeyTextBox.Text
     $Form.Controls.Add($NextButton)
 
     # Create the event handler for the API Key textbox
-    $SteamID64TextBox.add_TextChanged({
-            if ($SteamID64TextBox.Text.Length -eq 17) {
+    $ApiKeyTextBox.add_TextChanged({
+            if ($ApiKeyTextBox.Text.Length -eq 32) {
                 $NextButton.Enabled = $true
             }
             else {
@@ -213,19 +192,90 @@ if ($Result -eq [System.Windows.Forms.DialogResult]::OK -and $ApiKeyTextBox.Text
     $Result = $Form.ShowDialog()
 
     # If the "Next" button was clicked and the API Key is 32 characters long, set the $ApiKey variable
-    if ($Result -eq [System.Windows.Forms.DialogResult]::OK -and $SteamID64TextBox.Text.Length -eq 17) {
-        $SteamID64 = $SteamID64TextBox.Text
-        #URL for API
-        $apiUrl = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=$Apikey&steamid=$SteamID64&include_appinfo=1&format=json"
+    if ($Result -eq [System.Windows.Forms.DialogResult]::OK -and $ApiKeyTextBox.Text.Length -eq 32) {
+        $Apikey = $ApiKeyTextBox.Text
+        Add-Type -AssemblyName System.Windows.Forms
 
-        Add-ModuleExcel
-        
-    } else {
-        Clear-Installation
+        $Form = New-Object System.Windows.Forms.Form
+        $Form.Text = "Enter SteamID64"
+        $Form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog
+        $Form.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
+        $Form.AutoSize = $true
+        $form.TopMost = $true
+        $Form.AutoSizeMode = "GrowAndShrink"
+
+        # Create the message label
+        $MessageLabel = New-Object System.Windows.Forms.Label
+        $MessageLabel.Location = New-Object System.Drawing.Point(10, 10)
+        $MessageLabel.Size = New-Object System.Drawing.Size(280, 20)
+        $MessageLabel.Text = "Please enter your SteamID64 (must be 17 characters):"
+        $Form.Controls.Add($MessageLabel)
+
+        # Create the API Key textbox
+        $SteamID64TextBox = New-Object System.Windows.Forms.TextBox
+        $SteamID64TextBox.Location = New-Object System.Drawing.Point(10, 30)
+        $SteamID64TextBox.Size = New-Object System.Drawing.Size(280, 20)
+        $Form.Controls.Add($SteamID64TextBox)
+
+        # Create the "Next" button
+        $NextButton = New-Object System.Windows.Forms.Button
+        $NextButton.Location = New-Object System.Drawing.Point(115, 70)
+        $NextButton.Size = New-Object System.Drawing.Size(75, 23)
+        $NextButton.Text = "Next"
+        $NextButton.Enabled = $false
+        $NextButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+        $Form.AcceptButton = $NextButton
+        $Form.Controls.Add($NextButton)
+
+        # Create the event handler for the API Key textbox
+        $SteamID64TextBox.add_TextChanged({
+                if ($SteamID64TextBox.Text.Length -eq 17) {
+                    $NextButton.Enabled = $true
+                }
+                else {
+                    $NextButton.Enabled = $false
+                }
+            })
+
+        # Show the form and wait for a result
+        $Result = $Form.ShowDialog()
+
+        # If the "Next" button was clicked and the API Key is 32 characters long, set the $ApiKey variable
+        if ($Result -eq [System.Windows.Forms.DialogResult]::OK -and $SteamID64TextBox.Text.Length -eq 17) {
+            $SteamID64 = $SteamID64TextBox.Text
+            #URL for API
+            $apiUrl = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=$Apikey&steamid=$SteamID64&include_appinfo=1&format=json"
+            return $apiUrl
+        }
+        else {
+            Clear-Installation
+            exit
+        }
     }
-} else {
-    Clear-Installation
+    else {
+        Clear-Installation
+        exit
+    }
 }
+
+# Set the execution policy to be open temporarily
+Set-ExecutionPolicyTemporarily -ExecutionPolicy RemoteSigned -ScriptBlock {
+    # Prompt the user for their API credentials
+    Get-ApiAccess
+
+    # Grabs the list of Steam Games
+    Find-ModuleExcel
+
+    # Export the JSON file
+    Join-JsonTable
+    Write-Log "EXPORTED THE GAMES!"
+
+    # Open up file explorer for user
+    Invoke-Item -Path $tempDir
+
+    # Clean up after itself
+    Clear-Installation
+} 
 
 
 
