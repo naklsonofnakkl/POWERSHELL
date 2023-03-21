93d045e5cd7205f409b29836d1bbbdaa33498d0a
Andrew Wilson <naklsonofnakkl@gmail.com>
2023-03-20 23:14:30 -0400

    Birth of BBCC v0.0.0.0

---
+++ MICROSOFT/OFFICE/BBCC.psm1
@@ -0,0 +1,29 @@
+#Requires -Modules Appx
+#Requires -RunAsAdministrator
+
+<#
+.NOTES
+    Author: Andrew Wilson
+    Version: 0.0.0.0
+    
+.LINK
+    https://github.com/naklsonofnakkl/POWERSHELL
+
+.SYNOPSIS
+    The Big Beautiful Cache Clearing Module!
+.DESCRIPTION
+    - Clears the Cache of multiple office applications
+    - All packed into one easy to call module
+    - And at the total mercy of your imagination!
+
+.EXAMPLE
+
+
+#>
+
+<#
+--------------------
+FUNCTION JUNCTION!
+--------------------
+#>
+
